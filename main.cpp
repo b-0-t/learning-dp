b9880c13295d04d88d40576f3e95f964e3d4209a
dhruv <dhruvjn110327>
2024-03-26 19:11:53 +0530

    fibonacci memoization

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int f(int n, vector<int> &dp)
+{
+    if (n <= 1)
+        return n;
+    if (dp[n] != -1)
+        return dp[n];
+    return dp[n] = f(n - 1, dp) + f(n - 2, dp);
+}
+
 int main()
 {
-    cout << 12 << endl;
+    vector<int> dp(4, -1);
+
+    cout << f(4, dp);
     return 0;
 }

9c49adacffadd4f12c0930960925d6d6e721e48e
dhruv <dhruvjn110327>
2024-03-26 23:03:25 +0530

    tabulation method

--- main.cpp
+++ main.cpp
@@ -3,17 +3,20 @@ using namespace std;
 
 int f(int n, vector<int> &dp)
 {
-    if (n <= 1)
-        return n;
-    if (dp[n] != -1)
-        return dp[n];
-    return dp[n] = f(n - 1, dp) + f(n - 2, dp);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
 }
 
 int main()
 {
-    vector<int> dp(4, -1);
-
+    vector<int> dp(5, -1);
     cout << f(4, dp);
     return 0;
 }
